00c9c9f394f6d0ebede519726d8bfd5b131104c1
Amber Mohler <ambermohler@imac.lan>
2019-11-13 16:42:25 -0500

    finished all challenge questions

--- index.js
+++ index.js
@@ -28,27 +28,40 @@ Airplane.prototype.land = function () {
 
 /*
   TASK 1
-    - Write a Person Constructor that initializes `name` and `age` from arguments.
-    - All instances of Person should initialize with an empty `stomach` array.
-    - Give instances of Person the ability to `.eat("someFood")`:
+    ** Write a Person Constructor that initializes `name` and `age` from arguments.
+    ** All instances of Person should initialize with an empty `stomach` array.
+    **Give instances of Person the ability to `.eat("someFood")`:
         + When eating an edible, it should be pushed into the `stomach`.
         + The `eat` method should have no effect if there are 10 items in the `stomach`.
-    - Give instances of Person the ability to `.poop()`:
+    ** Give instances of Person the ability to `.poop()`:
         + When an instance poops, its `stomach` should empty.
-    - Give instances of Person a method `.toString()`:
+    ** Give instances of Person a method `.toString()`:
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.stomach = [];
+  this.name = name,
+  this.age = age
+}
+Person.prototype.eat = function(someFood){
+  if (this.stomach.length < 10){
+    this.stomach.push(someFood);
+  }
+}
+Person.prototype.poop = function(stomach){
+  this.stomach = [];
+}
+Person.prototype.toString = function(name, age){
+  return `${this.name} ${this.age}`;
 }
 
 /*
   TASK 2
-    - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
-    - All instances built with Car:
-        + should initialize with an `tank` at 0
-        + should initialize with an `odometer` at 0
+    ** Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
+    ** All instances built with Car:
+        ** should initialize with an `tank` at 0
+        ** should initialize with an `odometer` at 0
     - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
     - STRETCH: Give cars ability to `.drive(distance)`. The distance driven:
         + Should cause the `odometer` to go up.
@@ -57,20 +70,36 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.tank = 0,
+  this.odometer = 0,
+  this.model = model,
+  this.milesPerGallon = milesPerGallon
 }
+
+Car.prototype.fill = function (gallons) {
+  this.tank += gallons;
+}
+
 
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
-    - Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
+    **Besides `name` and `age`, Baby takes a third argument to initialize `favoriteToy`.
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  this.name = name,
+  this.age = age,
+  this.favoriteToy = favoriteToy
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+};
 
 /* 
   TASK 4
